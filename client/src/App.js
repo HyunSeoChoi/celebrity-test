7b2739c600cef48031bc29ab04f775e56ed64d86
HyunSeoChoi <arenaofjagal@gmail.com>
2019-09-15 02:02:41 +0900

    닮은꼴 확인 기능 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,75 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import { post } from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { file: "", fileName: "", similar: "", percent: "" };
+
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleFileChange = this.handleFileChange.bind(this);
+    this.handleFetch = this.handleFetch.bind(this);
+  }
+
+  handleFormSubmit(e) {
+    e.preventDefault();
+
+    this.handleFetch().then(response => {
+      this.setState({
+        similar: response.data.faces[0].celebrity.value,
+        percent: response.data.faces[0].celebrity.confidence * 100
+      });
+    });
+  }
+
+  handleFetch() {
+    const url = "/face";
+
+    const formData = new FormData();
+
+    formData.append("image", this.state.file);
+
+    const config = {
+      headers: {
+        "content-type": "multipart/form-data"
+      }
+    };
+
+    return post(url, formData, config);
+  }
+
+  handleFileChange(e) {
+    this.setState({
+      file: e.target.files[0],
+
+      fileName: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleFormSubmit}>
+          <h1>닮은꼴</h1>
+          <input
+            type="file"
+            name="file"
+            file={this.state.file}
+            value={this.state.fileName}
+            onChange={this.handleFileChange}
+          />
+
+          <button type="submit">보기</button>
+        </form>
+        {this.state.similar
+          ? this.state.similar +
+            "(이)라고 " +
+            this.state.percent +
+            "% 확신해요!"
+          : null}
+      </div>
+    );
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,12 +1,45 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
+const multer = require("multer");
+const upload = multer({
+  dest: "upload/",
+  limits: { fileSize: 2 * 1024 * 1024 }
+});
 const app = express();
 const port = process.env.PORT || 4000;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello Express!" });
+const data = fs.readFileSync("./config.json");
+const conf = JSON.parse(data);
+
+app.post("/face", upload.single("image"), (req, res) => {
+  //console.log(req);
+  var api_url = "https://openapi.naver.com/v1/vision/celebrity"; // 유명인 인식
+  //var api_url = 'https://openapi.naver.com/v1/vision/face'; // 얼굴 감지
+
+  var _formData = {
+    image: "image",
+    image: fs.createReadStream(__dirname + "/upload/" + req.file.filename) // FILE 이름
+  };
+  var _req = request
+    .post({
+      url: api_url,
+      formData: _formData,
+      headers: {
+        "X-Naver-Client-Id": conf.client_id,
+        "X-Naver-Client-Secret": conf.client_secret
+      }
+    })
+    .on("response", function(response) {
+      console.log(response.statusCode); // 200
+      console.log(response.headers["content-type"]);
+    });
+
+  _req.pipe(res); // 브라우저로 출력
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
