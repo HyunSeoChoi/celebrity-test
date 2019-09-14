9e2e8e9f8827292a234b42af8b330aa72dbb8c8b
HyunSeoChoi <arenaofjagal@gmail.com>
2019-09-15 02:34:12 +0900

    파일 크기 제한

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,13 @@ import { post } from "axios";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { file: "", fileName: "", similar: "", percent: "" };
+    this.state = {
+      file: "",
+      fileName: "",
+      similar: "",
+      percent: "",
+      nobody: false
+    };
 
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.handleFileChange = this.handleFileChange.bind(this);
@@ -15,10 +21,16 @@ class App extends Component {
     e.preventDefault();
 
     this.handleFetch().then(response => {
-      this.setState({
-        similar: response.data.faces[0].celebrity.value,
-        percent: response.data.faces[0].celebrity.confidence * 100
-      });
+      if (response.data.info.faceCount === 0) {
+        this.setState({
+          nobody: true
+        });
+      } else {
+        this.setState({
+          similar: response.data.faces[0].celebrity.value,
+          percent: response.data.faces[0].celebrity.confidence * 100
+        });
+      }
     });
   }
 
@@ -39,11 +51,21 @@ class App extends Component {
   }
 
   handleFileChange(e) {
-    this.setState({
-      file: e.target.files[0],
+    if (e.target.files[0].size > 1024 * 1024 * 2) {
+      // 용량 초과시 경고후 해당 파일의 용량도 보여줌
+      alert(
+        "2MB 이하 파일만 등록할 수 있습니다.\n\n" +
+          "현재파일 용량 : " +
+          Math.round((e.target.files[0].size / 1024 / 1024) * 100) / 100 +
+          "MB"
+      );
+    } else {
+      this.setState({
+        file: e.target.files[0],
 
-      fileName: e.target.value
-    });
+        fileName: e.target.value
+      });
+    }
   }
 
   render() {
@@ -61,6 +83,7 @@ class App extends Component {
 
           <button type="submit">보기</button>
         </form>
+        {this.state.nobody ? "닮은 사람이 없네요" : null}
         {this.state.similar
           ? this.state.similar +
             "(이)라고 " +
